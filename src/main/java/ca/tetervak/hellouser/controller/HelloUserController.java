0d13749177921936301471d1e8abfc7f7abb1d0e
Alex Tetervak <tetervak@sheridancollege.ca>
2021-01-22 13:04:16 -0500

    made the controller work

---
+++ src/main/java/ca/tetervak/hellouser/controller/HelloUserController.java
@@ -0,0 +1,19 @@
+package ca.tetervak.hellouser.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloUserController {
+
+    Logger logger = LoggerFactory.getLogger(HelloUserController.class);
+
+    @GetMapping(value={"/","/Output"})
+    public String output(){
+        logger.trace("output() is called");
+        return "Output";
+    }
+
+}
