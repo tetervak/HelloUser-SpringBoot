522dafd83725ae99c5127baba7d0c0d4636dd20c
Alex Tetervak <tetervak@sheridancollege.ca>
2021-01-22 13:23:19 -0500

    added the user class

--- src/main/java/ca/tetervak/hellouser/controller/HelloUserController.java
+++ src/main/java/ca/tetervak/hellouser/controller/HelloUserController.java
@@ -1,8 +1,10 @@
 package ca.tetervak.hellouser.controller;
 
+import ca.tetervak.hellouser.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -11,8 +13,17 @@ public class HelloUserController {
     Logger logger = LoggerFactory.getLogger(HelloUserController.class);
 
     @GetMapping(value={"/","/Output"})
-    public String output(){
+    public String output(Model model){
         logger.trace("output() is called");
+
+        User user = new User("Bart", "Simpson");
+
+        int rand = 1 + (int)(100*Math.random());
+        logger.debug("rand = " + rand);
+
+        model.addAttribute("user", user);
+        model.addAttribute("rand", rand);
+
         return "Output";
     }
 
